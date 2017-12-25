9b04763d3a4241e186fca10796d0d68ed4e005c0
Starrier <1342878298@qq.com>
2017-12-25 20:44:57 +0800

    add login model code

---
+++ login/src/main/java/com/example/starrier/login/model/LoginModel.java
@@ -0,0 +1,8 @@
+package com.example.starrier.login.model;
+
+/**
+ * Created by Starrier on 2017/12/25.
+ */
+
+public interface LoginModel {
+}
---
+++ login/src/main/java/com/example/starrier/login/presenter/LoginPresenter.java
@@ -0,0 +1,8 @@
+package com.example.starrier.login.presenter;
+
+/**
+ * Created by Starrier on 2017/12/25.
+ */
+
+public class LoginPresenter {
+}
