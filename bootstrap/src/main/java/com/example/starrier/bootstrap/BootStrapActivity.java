2ec167cf7370c40f939445bd9a03585748f57297
Starrier <1342878298@qq.com>
2017-12-25 20:32:59 +0800

    Add Module

---
+++ bootstrap/src/main/java/com/example/starrier/bootstrap/BootStrapActivity.java
@@ -0,0 +1,13 @@
+package com.example.starrier.bootstrap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BootStrapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot_strap);
+    }
+}
